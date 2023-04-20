cf0e55ba197674180550b9015dfd84c948ce3967
Alexandra Pilipyuk <aleksa-fleksa@ya.ru>
2023-04-20 10:07:12 +0200

    Changes in . pushed by gcommit

--- project.py
+++ project.py
@@ -2,12 +2,14 @@ import notify2
 import sys
 import os
 
+directory = '.'
 if len(sys.argv) > 1:
     directory = sys.argv[1]
-    os.system('git add ' + directory)
-    os.system('git commit -m "Changes in ' + directory + ' are pushed"')
-    os.system('git push')
 
-    notify2.init('app name')
-    n = notify2.Notification('GCommit', 'message')
-    n.show()
+os.system('git add ' + directory)
+os.system('git commit -m "Changes in ' + directory + ' pushed by gcommit"')
+os.system('git push')
+
+notify2.init('app name')
+n = notify2.Notification('GCommit', 'Changes in ' + directory + ' have been pushed')
+n.show()
